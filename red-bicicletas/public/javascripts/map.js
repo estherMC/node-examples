ce2d8a973b6921dc2b370380f73ba532918c70f4
EstherMC <esther_3@hotmail.com>
2020-08-23 22:50:40 +0200

    :sparkles: Add map and template to project

---
+++ red-bicicletas/public/javascripts/map.js
@@ -0,0 +1,17 @@
+var map = L.map('main_map').setView([41.1257995, 1.2035639], 12);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([41.1518132, 1.2059955]).addTo(map)
+    .bindPopup('Constantí')
+    .openPopup();
+    
+L.marker([41.1504585, 1.0721486]).addTo(map)
+    .bindPopup('Reus')
+    .openPopup();
+
+L.marker([41.1257995, 1.2035639]).addTo(map)
+    .bindPopup('Tarragona')
+    .openPopup();
