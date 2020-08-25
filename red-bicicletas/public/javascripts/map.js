77ca7217e79c2ebbefc981599e9f29064014d707
EstherMC <esther_3@hotmail.com>
2020-08-25 23:34:39 +0200

    :hammer: Add API endpoints

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -4,7 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([41.1518132, 1.2059955]).addTo(map)
+/* L.marker([41.1518132, 1.2059955]).addTo(map)
     .bindPopup('Constantí')
     .openPopup();
     
@@ -14,4 +14,15 @@ L.marker([41.1504585, 1.0721486]).addTo(map)
 
 L.marker([41.1257995, 1.2035639]).addTo(map)
     .bindPopup('Tarragona')
-    .openPopup();
+    .openPopup(); */
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function (result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+});
